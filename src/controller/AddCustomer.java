b5a2331804c3802ebc6dc02d42a4b598e2bff4b3
chrisram30 <chris.ram30@gmail.com>
2022-09-10 02:30:06 -0600

    test

--- src/controller/AddCustomer.java
+++ src/controller/AddCustomer.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 public class AddCustomer {
+    public Button saveButton;
+    public Button cancelButton;
+    public TextField customerIdBox;
+    public TextField customerNameBox;
+    public TextField addressBox;
+    public TextField postalCodeBox;
+    public TextField phoneBox;
+    public TextField stateProvinceBox;
+    public MenuButton CountryComboBox;
+
+    public void saveButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void countryComboBoxClick(ActionEvent actionEvent) {
+    }
 }
